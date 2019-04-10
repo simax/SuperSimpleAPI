acf5a862d5ad8d028125f07dc9633e48ed9e52d6
simax <simonlomax@ekm.com>
2019-04-10 09:20:20 +0100

    Initial commit

---
+++ SuperSimpleAPI/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace SuperSimpleAPI
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+        public static HttpMessageHandler BackChannelHandler { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            ConfigureAuth(services);
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+        }
+
+        public virtual void ConfigureAuth(IServiceCollection services)
+        {
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddIdentityServerAuthentication(options =>
+            //    {
+            //        options.Authority = Configuration.GetValue<string>("IdentityServerAuthority");
+            //        options.Audience = Configuration.GetValue<string>("IdentityServerAudience");
+            //        options.BackchannelHttpHandler = BackChannelHandler;
+            //    });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration.GetValue<string>("IdentityServerAuthority");
+                    options.Audience = Configuration.GetValue<string>("IdentityServerAudience");
+                    options.BackchannelHttpHandler = BackChannelHandler;
+                });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
